36c2b9e2216b7289caa630396e27573567d539c8
Marcel Widmer <marzelwidmer@gmail.com>
2016-03-21 21:37:48 +0100

    part 2

---
+++ src/main/java/ch/keepcalm/web/config/WebConfiguration.java
@@ -0,0 +1,20 @@
+package ch.keepcalm.web.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by marcelwidmer on 21/03/16.
+ * The following Spring Configuration declares the servlet wrapper for the H2 database console and maps it to the path of /console.
+ */
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
