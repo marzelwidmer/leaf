49636e0f79eb3fe971669d0f2e2b82f3843298f1
Marcel Widmer <marzelwidmer@gmail.com>
2016-03-21 22:01:10 +0100

    JUnit ProductRepositoryTest

---
+++ src/test/java/ch/keepcalm/web/repositories/ProductRepositoryTest.java
@@ -0,0 +1,85 @@
+package ch.keepcalm.web.repositories;
+
+import ch.keepcalm.web.configuration.RepositoryConfiguration;
+import ch.keepcalm.web.domain.Product;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.math.BigDecimal;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by marcelwidmer on 21/03/16.
+ *
+ * <ul>
+ *     <li>Save an entity</li>
+ *     <li>Find an entity based on its ID</li>
+ *     <li>Check if an entity exists based on its ID</li>
+ *     <li>Get a list of all entities</li>
+ *     <li>Get a count of all entities</li>
+ *     <li>Delete an entity</li>
+ *     <li>Delete all entities</li></li>
+ * </ul>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {RepositoryConfiguration.class})
+public class ProductRepositoryTest {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Test
+    public void testSaveProduct(){
+        //setup product
+        Product product = new Product();
+        product.setDescription("Spring Framework Guru Shirt");
+        product.setPrice(new BigDecimal("18.95"));
+        product.setProductId("1234");
+
+        //save product, verify has ID value after save
+        assertNull(product.getId()); //null before save
+        productRepository.save(product);
+        assertNotNull(product.getId()); //not null after save
+
+        //fetch from DB
+        Product fetchedProduct = productRepository.findOne(product.getId());
+
+        //should not be null
+        assertNotNull(fetchedProduct);
+
+        //should equal
+        assertEquals(product.getId(), fetchedProduct.getId());
+        assertEquals(product.getDescription(), fetchedProduct.getDescription());
+
+        //update description and save
+        fetchedProduct.setDescription("New Description");
+        productRepository.save(fetchedProduct);
+
+        //get from DB, should be updated
+        Product fetchedUpdatedProduct = productRepository.findOne(fetchedProduct.getId());
+        assertEquals(fetchedProduct.getDescription(), fetchedUpdatedProduct.getDescription());
+
+        //verify count of products in DB
+        long productCount = productRepository.count();
+        assertEquals(productCount, 1);
+
+        //get all products, list should only have one
+        Iterable<Product> products = productRepository.findAll();
+
+        int count = 0;
+
+        for(Product p : products){
+            count++;
+        }
+
+        assertEquals(count, 1);
+    }
+}
