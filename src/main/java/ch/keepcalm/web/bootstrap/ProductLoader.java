ca56b368befb270198dff92d70d177e17f6407f4
Marcel Widmer <marzelwidmer@gmail.com>
2016-03-21 22:18:06 +0100

    add product loader, application properties files for dev, local

---
+++ src/main/java/ch/keepcalm/web/bootstrap/ProductLoader.java
@@ -0,0 +1,55 @@
+package ch.keepcalm.web.bootstrap;
+
+
+import ch.keepcalm.web.domain.Product;
+import ch.keepcalm.web.repositories.ProductRepository;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+
+/**
+ * Created by marcelwidmer on 21/03/16.
+ *
+ * This class implements the ApplicationListner interface, so it gets called upon the ContextRefresedEvent on startup.
+ * We’re using Spring to inject the Spring Data JPA repository into the class for our use.
+ * In this example, I’m creating two entities and saving them to the database.
+ */
+@Component
+public class ProductLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+    private ProductRepository productRepository;
+
+    private Logger log = Logger.getLogger(ProductLoader.class);
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        Product shirt = new Product();
+        shirt.setDescription("Spring Framework Guru Shirt");
+        shirt.setPrice(new BigDecimal("18.95"));
+        shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
+        shirt.setProductId("235268845711068308");
+        productRepository.save(shirt);
+
+        log.info("Saved Shirt - id: " + shirt.getId());
+
+        Product mug = new Product();
+        mug.setDescription("Spring Framework Guru Mug");
+        mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
+        mug.setProductId("168639393495335947");
+        mug.setPrice(new BigDecimal("11.95"));
+        productRepository.save(mug);
+
+        log.info("Saved Mug - id:" + mug.getId());
+    }
+}
