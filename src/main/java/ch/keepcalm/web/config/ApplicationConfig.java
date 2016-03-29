a0e0b2d9a219351e59e33829aed30f599e86358b
Marcel Widmer <marzelwidmer@gmail.com>
2016-03-29 21:24:26 +0200

    add h2 console menu point and version in head in non prod profile

---
+++ src/main/java/ch/keepcalm/web/config/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package ch.keepcalm.web.config;
+
+/**
+ * Created by marcelwidmer on 29/03/16.
+ */
+
+/*@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan
+public class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}*/
+/*
+@Configuration
+@ComponentScan
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setLocation(new ClassPathResource("application.properties"));
+        return c;
+    }
+
+}
+*/
