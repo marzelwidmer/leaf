6744531ee275a1cfb7d2d02487bb57aba6e7b2a5
Marcel Widmer <marzelwidmer@gmail.com>
2016-03-31 09:41:59 +0200

    change default error page

---
+++ src/main/java/ch/keepcalm/web/config/ApplicationConfiguration.java
@@ -0,0 +1,47 @@
+package ch.keepcalm.web.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Created by marcelwidmer on 21/03/16.
+ */
+@Configuration
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
+
+    /**
+     * The following Spring Configuration declares the servlet wrapper for the H2 database console and maps it to the path of /console.
+     *
+     * @return a h2 WebServlet
+     */
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
