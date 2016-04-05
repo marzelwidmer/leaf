03d40df0856248748b58a205fb8798917ed1d46e
Marcel Widmer <marzelwidmer@gmail.com>
2016-04-05 22:05:36 +0200

    separate SQL script add flyway.clean-on-validation-error=true rename test config package (cherry picked from commit b59de33)

---
+++ src/main/resources/db/migration/V2__insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO product
+  (description,
+  image_url,
+  price,
+  product_id)
+VALUES
+  ('Spring Framework Guru Shirt',
+  'https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg',
+  18.95,
+  '2352688457110683099');
+
+INSERT INTO product
+  (description,
+  image_url,
+  price,
+  product_id)
+VALUES
+  ('Spring Framework Guru Mug',
+  'https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg',
+  11.95,
+  '168639393495335947');
